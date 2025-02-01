d586cbca00bd530d438f21ead36ecad82a3aee6a
Chie Nakagawa <chie.design22@gmail.com>
2025-02-01 13:05:28 +0900

    cssトップページ全体導入

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import { posts } from './date/posts';
+import classes from './css/ArticleList.module.css';
 
 export const ArticleList = () => {
   return (
-    <div className="container">
+    <div className={classes.container}>
       <ul>
         {
           // mapメソッドで記事データを取得し繰り返し表示
           posts.map(post => (
-            <li key={post.id} className="postList">
-              <div className="postWrapper">
+            <li key={post.id} className={classes.postList}>
+              <div className={classes.postWrapper}>
                 {/* 日付をJavascriptのnew Date()で返し、toLocaleDateString()で整えて表示する */}
-                <time className="postDate">{new Date(post.createdAt).toLocaleDateString()}</time>
+                <time className={classes.postDate}>{new Date(post.createdAt).toLocaleDateString()}</time>
                 {/* カテゴリーをmapメソッドで取得し表示する */}
                 <div>
                   {post.categories.map((category) => {
                     return (
-                      <span key={category} className="postCategory">{ category}</span>
+                      <span key={category} className={classes.postCategory}>{category}</span>
                     );
                   })}
                 </div>
               </div>
               <h2>{post.title}</h2>
               {/* ReactでHTMLをそのまま表示:dangerouslySetInnerHTML属性を使用 */}
-              <p className="postContent" dangerouslySetInnerHTML={{ __html:post.content }}></p>
+              <p className={classes.postContent} dangerouslySetInnerHTML={{ __html:post.content }}></p>
             </li>
         ))}
       </ul>
