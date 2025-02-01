32578b8c0290de73a30f59eb86b7f7a5f71320f1
Chie Nakagawa <chie.design22@gmail.com>
2025-02-01 12:24:11 +0900

    CSSmodule導入Headercss導入

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './css/Header.module.css';
 
 export const Header = () => {
   return (
-    <header className="header">
+    <header className={classes.header}>
       <ul>
         <li><a href="/">Blog</a></li>
         <li><a href="#">お問い合わせ</a></li>
