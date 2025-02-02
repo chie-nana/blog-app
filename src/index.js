29a283cc5acd75129cb649df5874b4d7f44d11dd
Chie Nakagawa <chie.design22@gmail.com>
2025-02-02 13:48:07 +0900

    index BrowserRouter追加設定

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
