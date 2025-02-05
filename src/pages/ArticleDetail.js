2136af461f1b10244e88fa974ad0ede92e9eac92
Chie Nakagawa <chie.design22@gmail.com>
2025-02-05 23:43:34 +0900

    DetailページthumbnailUrl追加

--- src/pages/ArticleDetail.js
+++ src/pages/ArticleDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { posts } from '../data/posts';
+import classes from '../css/ArticleDetail.module.css';
 
 export const ArticleDetail = () => {
   const { id } = useParams();
@@ -11,9 +12,16 @@ export const ArticleDetail = () => {
   }
 
   return (
-    <div>
-      <h2>{post.title}</h2>
-      <p>{post.content}</p>
-    </div>
+    <>
+      <div className={classes.detailContainer}>
+        <div className={classes.detailMain}>
+          <div className={classes.detailImgBox}>
+            <img className={classes.detailImg} src={post.thumbnailUrl} alt={post.title} />
+          </div>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      </div>
+    </>
   );
 };
